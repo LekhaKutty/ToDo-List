28369c94c13ad018524aefce7b0ef9a62f251b02
Lekha Ajith Kutty <ss.lekha@gmail.com>
2020-03-30 22:54:17 +0300

    added bootstrap

---
+++ todoapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>To Do App</h1>
+        <button className="btn btn-warning">Button</button>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
